8128ecd8feb76452093b9b6c9dddde1a0516c908
fbarikzehi <f.barikzehi.dev@gmail.com>
2023-02-21 01:08:52 +0330

    Add razor pages runtime compilation

--- front/Program.cs
+++ front/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+var viewsBuilder = builder.Services.AddControllersWithViews();
+
+
+if (builder.Environment.IsDevelopment())
+{
+    viewsBuilder.AddRazorRuntimeCompilation();
+}
 
 var app = builder.Build();
 
