eeeab397032a22463b462f07860e9eeaac02cb31
fbarikzehi <f.barikzehi.dev@gmail.com>
2023-02-22 23:31:07 +0330

    add MongoDB settings

---
+++ Data/Data.Persistence/MongoDBSettings.cs
@@ -0,0 +1,11 @@
+
+namespace Data.Persistence;
+
+public class MongoDBSettings
+{
+
+    public string ConnectionURI { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+
+}
